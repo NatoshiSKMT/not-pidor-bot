441f8b2c882b6481b21ba4f4a8bc6adef21bc8b0
NatoshiSKMT <natoshi-sakamoto2@protonmail.com>
2021-08-02 16:17:26 +0700

    main handler

--- main.py
+++ main.py
@@ -6,20 +6,21 @@ except Exception as e:
     raise ValueError("Create your config.yml via 'cp config.yml.example config.yml && nano config.yml'")
 
 import mysql.connector
-mydb = mysql.connector.connect(
+db = mysql.connector.connect(
     host=config['host'],
     user=config['user'],
     database=config['database'],
     password=config['password']
 )
-cursor = mydb.cursor()
+cursor = db.cursor()
 
-from telegram.ext import Updater
+from telegram.ext import Updater, MessageHandler, Filters
 updater = Updater(token=config['token'])
 updater.start_polling()
 dispatcher = updater.dispatcher
 
 from datetime import datetime
+import re
 
 def find_plural(word):
     startTime = datetime.now()
@@ -39,15 +40,41 @@ def find_plural(word):
     print("SEARCH: " + word + " {}".format(datetime.now() - startTime))
     return plural
 
-def echo(update, context):
+def pidor(update, context):
+    #Saving a message
+    sql = "INSERT INTO `messages` (`text`, `tg_chat_id`, `tg_from_id`, `tg_from_username`) VALUES (%s, %s, %s, %s)"
+    cursor.execute(sql, (update.message.text, update.message.chat.id, update.message.from_user.id, update.message.from_user.username))
+    db.commit()
+    message_id = cursor.lastrowid
+
+    #Static replies
+    #check personal timeout
+    ###...
+
+    clear_text = re.sub("[^a-zA-Zа-яА-Я- ]+", "", update.message.text).lower()
+
+    if clear_text.endswith('для пидоров'):
+        update.message.reply_text('+', quote = True)
+        return
+
+    for word in clear_text.split():
+        replay_word = find_plural(word);
+        if replay_word != False:
+            chat_id = update.effective_chat.id
+            update.message.reply_text(replay_word.title() + ' для пидоров', quote = True)
+            return
+
+def admin(update, context):
     for item in update.message.text.split():
         item = item.lower()
         replay_word = find_plural(item);
         if replay_word != False:
-            #chat_id = update.effective_chat.id
-            chat_id = 127529747
-            context.bot.send_message(chat_id, text = replay_word.title() + ' для пидоров')
+            chat_id = update.effective_chat.id
+            update.message.reply_text(replay_word.title() + ' для пидоров', quote = True)
 
-from telegram.ext import MessageHandler, Filters
-echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-dispatcher.add_handler(echo_handler)
+if config['admin_chat_id'] > 0:
+    admin_handler = MessageHandler(Filters.chat(config['admin_chat_id']), admin)
+    dispatcher.add_handler(admin_handler)
+
+pidor_handler = MessageHandler(Filters.text & (~Filters.command), pidor)
+dispatcher.add_handler(pidor_handler)
